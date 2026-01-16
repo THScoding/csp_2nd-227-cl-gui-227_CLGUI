b3c9b6f570bcdd09c14cee4173761f17050a1e5e
Elliot <Elliot.olmstead@crpusd.org>
2026-01-16 12:09:09 -0800

    added fish

--- p227_starter_one_button_butmacaroni.py
+++ p227_starter_one_button_butmacaroni.py
@@ -94,5 +94,6 @@ command_textbox.pack()
 # Makes the command button pass it's name to a function using lambda
 ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", command=lambda:do_command("ping"))
 ping_btn.pack()
-
+ 
+print("FISH")
 root.mainloop()
